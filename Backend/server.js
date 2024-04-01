c8354ae4ca7f6beafd60640cc108f4b1a40e6c82
Abhi-rangi <abhishekrangi9@gmail.com>
2024-04-01 17:26:42 -0400

    update search vehicleType

--- Backend/server.js
+++ Backend/server.js
@@ -29,11 +29,35 @@ app.get("/", (req, res) => {
 });
 
 app.get("/vehicles", (req, res) => {
-  db.query("SELECT * FROM vehicle", (error, results) => {
-    if (error) throw error;
-    res.json(results);
-  });
+ const vtype = req.query.vtype; // Retrieve the vtype query parameter from the request
+
+ let query = "SELECT * FROM vehicle";
+ let params = [];
+
+ if (vtype) {
+   query += " WHERE vtype = ?";
+   params.push(vtype);
+ }
+
+ db.query(query, params, (error, results) => {
+   if (error) {
+     console.error("Error executing query:", error);
+     return res.status(500).json({ error: "Internal server error" });
+   }
+   res.json(results);
+ });
 });
+// Endpoint to fetch distinct vehicle types
+app.get('/vehicle-types', (req, res) => {
+    db.query('SELECT DISTINCT vtype FROM vehicle', (error, results) => {
+        if (error) {
+            console.error('Error fetching vehicle types:', error);
+            return res.status(500).send('Error fetching vehicle types');
+        }
+        res.json(results);
+    });
+});
+
 
 // Start the server
 app.listen(port, () => {
